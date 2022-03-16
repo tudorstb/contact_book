843937ce188bfc1d9084d7b85d1ec62244ee9c9c
tudorstb <tudor.stanescu@student.upt.ro>
2022-03-16 17:49:26 +0200

    intro

--- contact_book_project.py
+++ contact_book_project.py
@@ -1,1 +1,11 @@
-print("                 Contact book                ")
+def intro(name):
+    print("  ___________________________________________ ")
+    print("                Contact book                ")
+    print("  ___________________________________________ ")
+    print(f"                 Owner:{name}                ")
+    print("  ___________________________________________ ")
+
+name=input('To open a new contact book:')
+intro(name)
+with open('readme.txt', 'a') as f:
+    f.write(name)
